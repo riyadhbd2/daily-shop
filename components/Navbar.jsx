4632d19e90f543f7f936937d0d096d2a80b2e798
Easir Arafat <riyadhbd2@Easirs-MacBook-Air.local>
2025-06-13 17:54:49 +0200

    forntend done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,10 +4,13 @@ import { assets} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
+import { useClerk } from "@clerk/nextjs";
 
 const Navbar = () => {
 
   const { isSeller, router } = useAppContext();
+
+  const {openSignIn} = useClerk();
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
